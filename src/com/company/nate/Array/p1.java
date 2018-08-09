53ffbedb562e00994fc59a34327ca49b08814dba
nate <>
2018-08-09 14:58:49 +0800

    first init

---
+++ src/com/company/nate/Array/p1.java
@@ -0,0 +1,21 @@
+package com.company.nate.Array;
+
+public class p1 {
+
+
+    static public int removeDuplicates(int[] nums) {
+
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+
+        int[] intArray = {0,0,1,1,1,2,2,3,3,4};
+        int res = removeDuplicates(intArray);
+        System.out.println(res);
+    }
+
+}
---
+++ src/com/company/nate/Main.java
@@ -0,0 +1,8 @@
+package com.company.nate;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
