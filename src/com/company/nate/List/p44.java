bba397ed5d3853a6aedf3057e313d472cfda9b39
wangxu <xwangx@ctrip.com>
2018-12-20 19:12:09 +0800

    44

--- src/com/company/nate/List/p44.java
+++ src/com/company/nate/List/p44.java
@@ -16,32 +16,60 @@ public class p44 {
 
         ListNode node;
 
-        if (l1.val > )
-
-        while (l1 != null || l2 != null) {
-
-            if (l1 == null) {
-
-            }
-
-            if (l2 == null) {
-
-            }
-
-
-
-
-
+        if (l1.val > l2.val) {
+            node = l2;
+            node.next = l1;
+        } else {
+            node = l1;
+            node.next = l2;
         }
 
+        ListNode node1 = l1.next;
+        ListNode node2 = l2.next;
 
+        ListNode headNode = node;
 
+        while (node1 != null || node2 != null) {
 
+            if (node1 == null) {
+               node.next = node2 ;
 
+                node1 = node1.next;
+                node2 = node2.next;
 
+                node.next.next = null;
+                continue;
+            }
 
+            if (node2 == null) {
+                node.next = node1;
 
+                node1 = node1.next;
+                node2 = node2.next;
 
+                node.next.next = null;
+                continue;
+            }
+
+            if (node1.val > node2.val) {
+                node.next = node2 ;
+                node.next.next = node1;
+
+                node1 = node1.next;
+                node2 = node2.next;
+                node.next.next = null;
+            } else {
+                node.next = node1;
+                node.next.next = node2;
+
+                node1 = node1.next;
+                node2 = node2.next;
+
+                node.next.next = null;
+            }
+        }
+
+        return headNode;
     }
 
     public static void main(String[] args) {
@@ -59,9 +87,21 @@ public class p44 {
             pNode = node;
         }
 
-        head = mergeTwoLists(head, head);
 
-        head.print();
+        ListNode head2 = new ListNode(arr[0]);
+        pNode = head2;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            ListNode node = new ListNode(arr[i]);
+
+            pNode.next = node;
+            pNode = node;
+        }
+
+        ListNode mergeHead = mergeTwoLists(head, head2);
+
+        mergeHead.print();
     }
 
 }
